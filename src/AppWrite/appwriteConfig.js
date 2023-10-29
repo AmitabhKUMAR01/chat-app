1ffc4e4a3a1b07cb574a2c548933a8908acc44ea
amitabh <amitabhkumar7668@gmail.com>
2023-10-29 15:35:18 +0530

    again same problem

--- src/AppWrite/appwriteConfig.js
+++ src/AppWrite/appwriteConfig.js
@@ -26,7 +26,7 @@ export const account = new Account(client);
 export const storage = new Storage(client);
 export default client;
 account.updatePrefs({
-    origin: ['https://main--melodious-pavlova-a5baab.netlify.app']
+    origin: ['https://creative-gaufre-75e3c7.netlify.app/']
   })
     .then(response => {
       console.log('CORS settings updated successfully',response);
