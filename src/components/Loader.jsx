6f1ac5b18dcba31f1cfdcf8f5ad3edbb7ed4aed0
AmitabhKUMAR01 <amitabhkumar7668@gmail.com>
2024-01-31 14:49:30 +0530

    changed loader bg color

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,11 +2,11 @@ import { Triangle } from "react-loader-spinner"
 
 const Loader = () => {
   return (
-    <div className="w-[100vw] h-[100vh] grid justify-center text-center items-center " style={{background:' #031731 '}} >
+    <div className="w-[100vw] h-[100vh] grid justify-center text-center items-center " style={{background:' transparent'}} >
         <Triangle
       height="80"
       width="80"
-      color="#4FC0D0"
+      color="black"
       ariaLabel="triangle-loading"
       wrapperStyle={{}}
       wrapperClassName=""
